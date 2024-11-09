7a9078b8ecafa5e1bced7d1a7a3cc1224f2cf780
iamdbj <zzang9083@naver.com>
2024-11-10 00:52:22 +0900

    commit

--- src/main/java/euljiro/project/childcareproducts/common/response/CommonControllerAdvice.java
+++ src/main/java/euljiro/project/childcareproducts/common/response/CommonControllerAdvice.java
@@ -2,7 +2,6 @@ package euljiro.project.childcareproducts.common.response;
 
 import com.google.common.collect.Lists;
 import euljiro.project.childcareproducts.common.exception.BaseException;
-import euljiro.project.childcareproducts.common.interceptor.CommonHttpRequestInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.ClientAbortException;
 import org.slf4j.MDC;
@@ -35,8 +34,6 @@ public class CommonControllerAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = Exception.class)
     public CommonResponse onException(Exception e) {
-        String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
-        log.error("eventId = {} ", eventId, e);
         return CommonResponse.fail(ErrorCode.COMMON_SYSTEM_ERROR);
     }
 
@@ -51,12 +48,7 @@ public class CommonControllerAdvice {
     @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(value = BaseException.class)
     public CommonResponse onBaseException(BaseException e) {
-        String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
-        if (SPECIFIC_ALERT_TARGET_ERROR_CODE_LIST.contains(e.getErrorCode())) {
-            log.error("[BaseException] eventId = {}, cause = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e), NestedExceptionUtils.getMostSpecificCause(e).getMessage());
-        } else {
-            log.warn("[BaseException] eventId = {}, cause = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e), NestedExceptionUtils.getMostSpecificCause(e).getMessage());
-        }
+
         return CommonResponse.fail(e.getErrorCode().getErrorMsg(), e.getErrorCode().name());
     }
 
@@ -71,8 +63,6 @@ public class CommonControllerAdvice {
     @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(value = {ClientAbortException.class})
     public CommonResponse skipException(Exception e) {
-        String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
-        log.warn("[skipException] eventId = {}, cause = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e), NestedExceptionUtils.getMostSpecificCause(e).getMessage());
         return CommonResponse.fail(ErrorCode.COMMON_SYSTEM_ERROR);
     }
 
@@ -87,8 +77,6 @@ public class CommonControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
     public CommonResponse methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        String eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY);
-        log.warn("[BaseException] eventId = {}, errorMsg = {}", eventId, NestedExceptionUtils.getMostSpecificCause(e).getMessage());
         BindingResult bindingResult = e.getBindingResult();
         FieldError fe = bindingResult.getFieldError();
         if (fe != null) {
